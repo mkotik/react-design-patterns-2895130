03fe540983e242485565ee7007af1e37e2a46d59
Marat Kotik <mkotik@mjels.com>
2022-01-27 17:28:26 -0500

    final

--- src/RegularList.js
+++ src/RegularList.js
@@ -1,13 +1,13 @@
 export const RegularList = ({
-	items,
-	resourceName,
-	itemComponent: ItemComponent,
+  items,
+  resourceName,
+  itemComponent: ItemComponent,
 }) => {
-	return (
-		<>
-		{items.map((item, i) => (
-			<ItemComponent key={i} {...{ [resourceName]: item }} />
-		))}
-		</>
-	)
-}
+  return (
+    <>
+      {items.map((item, i) => (
+        <ItemComponent key={i} {...{ [resourceName]: item }} />
+      ))}
+    </>
+  );
+};
