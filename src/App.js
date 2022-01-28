e8c3990283bc6f6e95d778071f09f1940a48883b
Marat Kotik <mkotik@mjels.com>
2022-01-28 10:05:06 -0500

    final

--- src/App.js
+++ src/App.js
@@ -4,62 +4,63 @@ import { SmallPersonListItem } from "./people/SmallPersonListItem";
 import { LargeProductListItem } from "./products/LargeProductListItem";
 import { SmallProductListItem } from "./products/SmallProductListItem";
 import { RegularList } from "./RegularList";
+import { Modal } from "./Modal.js";
 
-const people = [{
-	name: 'John Doe',
-	age: 54,
-	hairColor: 'brown',
-	hobbies: ['swimming', 'bicycling', 'video games'],
-}, {
-	name: 'Brenda Smith',
-	age: 33,
-	hairColor: 'black',
-	hobbies: ['golf', 'mathematics'],
-}, {
-	name: 'Jane Garcia',
-	age: 27,
-	hairColor: 'blonde',
-	hobbies: ['biology', 'medicine', 'gymnastics'],
-}];
+const people = [
+  {
+    name: "John Doe",
+    age: 54,
+    hairColor: "brown",
+    hobbies: ["swimming", "bicycling", "video games"],
+  },
+  {
+    name: "Brenda Smith",
+    age: 33,
+    hairColor: "black",
+    hobbies: ["golf", "mathematics"],
+  },
+  {
+    name: "Jane Garcia",
+    age: 27,
+    hairColor: "blonde",
+    hobbies: ["biology", "medicine", "gymnastics"],
+  },
+];
 
-const products = [{
-	name: 'Flat-Screen TV',
-	price: '$300',
-	description: 'Huge LCD screen, a great deal',
-	rating: 4.5,
-}, {
-	name: 'Basketball',
-	price: '$10',
-	description: 'Just like the pros use',
-	rating: 3.8,
-}, {
-	name: 'Running Shoes',
-	price: '$120',
-	description: 'State-of-the-art technology for optimum running',
-	rating: 4.2,
-}];
+const products = [
+  {
+    name: "Flat-Screen TV",
+    price: "$300",
+    description: "Huge LCD screen, a great deal",
+    rating: 4.5,
+  },
+  {
+    name: "Basketball",
+    price: "$10",
+    description: "Just like the pros use",
+    rating: 3.8,
+  },
+  {
+    name: "Running Shoes",
+    price: "$120",
+    description: "State-of-the-art technology for optimum running",
+    rating: 4.2,
+  },
+];
 
 function App() {
-	return (
-		<>
-		<RegularList
-			items={people}
-			resourceName="person"
-			itemComponent={SmallPersonListItem} />
-		<NumberedList
-			items={people}
-			resourceName="person"
-			itemComponent={LargePersonListItem} />
-		<RegularList
-			items={products}
-			resourceName="product"
-			itemComponent={SmallProductListItem} />
-		<NumberedList
-			items={products}
-			resourceName="product"
-			itemComponent={LargeProductListItem} />
-		</>
-	);
+  return (
+    <>
+      <Modal className="modal">
+        <RegularList
+          items={products}
+          resourceName="product"
+          itemComponent={SmallProductListItem}
+          className="regular-list"
+        />
+      </Modal>
+    </>
+  );
 }
 
 export default App;
